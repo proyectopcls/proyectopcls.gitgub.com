4b5b9414b03adcf4d4a8250b61ed578645bf3340
proyectopcls <93728793+proyectopcls@users.noreply.github.com>
2021-11-04 10:46:25 -0600

    Update ctrlP.js

--- js/ctrlP.js
+++ js/ctrlP.js
@@ -1,1 +1,26 @@
+"use strict";
+var forma = document.getElementById ("forma"),
+     salidaP = document.getElementById("salidaP");
+forma.addEventListener("submit",procesa,false);
 
+function procesa(){
+    var base =parseInt( forma["base"].value);
+    var res="";
+    for (var i = 0; i <= 10; i++) {
+         res=res+ base +"^" +i+ "="+potencia(base,i)+"\n";
+        
+    }
+   forma["salidaP"].value=res;
+}
+
+function potencia(num1,num2){
+     const base = num1;
+          const exponente = num2;
+          let r = 1;
+          let i = 0; 
+          while(i < exponente){
+              r = r * base;
+              i++;
+          }
+          return (r);
+}
